0147d64f8ae758fcd5c977e4aebe4902716774a2
koogar <l33t@ntlworld.com>
2021-03-12 17:03:38 +0000

    Update GnatStats_ATSAMD_QTPY_OLED_V1.33.ino

--- OLED_GnatStats/GnatStats_ATSAMD_QTPY_OLED_V1.33/GnatStats_ATSAMD_QTPY_OLED_V1.33.ino
+++ OLED_GnatStats/GnatStats_ATSAMD_QTPY_OLED_V1.33/GnatStats_ATSAMD_QTPY_OLED_V1.33.ino
@@ -55,6 +55,9 @@
   Adafruit SSD1306 library
   https://github.com/adafruit/Adafruit_SSD1306
 
+  SH1106 Library
+  Not supported on the ATSAMD!!!
+
   Adafruit GFX Library
   https://github.com/adafruit/Adafruit-GFX-Library
 
